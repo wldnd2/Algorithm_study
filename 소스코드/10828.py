c807bb04d1c39f2ab459e9d6fa967c173d8153f9
wldnd2 <jiwoong000628@gmail.com>
2022-07-07 23:18:30 +0900

    10828 스택

---
+++ 소스코드/10828.py
@@ -0,0 +1,25 @@
+# 10828 스택
+import sys
+input = sys.stdin.readline
+stack = []
+for _ in range(int(input())):
+    command = input().split()
+    if command[0] == "push":
+        stack.append(int(command[1]))
+    elif command[0] == "pop":
+        if len(stack) == 0:
+            print("-1")
+        else:
+            print(stack.pop())
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if len(stack) == 0:
+            print("1")
+        else:
+            print("0")
+    elif command[0] == "top":
+        if len(stack) == 0:
+            print("-1")
+        else:
+            print(stack[-1])
